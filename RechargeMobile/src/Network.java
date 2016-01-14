e50e6c1eb4b3460b1fab4b63df23af96c286ea58
praveen <pravinppms@gmail.com>
2016-01-14 18:37:00 +0530

    First Change

--- RechargeMobile/src/Network.java
+++ RechargeMobile/src/Network.java
@@ -3,5 +3,7 @@ public class Network {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello.., This is Idea Network");
+		System.out.println("One Idea Can change Your Life");
+		
 	}
 }
